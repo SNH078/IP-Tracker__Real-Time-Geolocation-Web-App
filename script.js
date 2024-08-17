f3c34061b29089a6f2f79aa935a2067238a4397c
SNH115577 <164773937+SNH078@users.noreply.github.com>
2024-08-17 14:52:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let map, marker;
 const ipGeoApiKey = import.meta.env.VITE_IPGEOLOCATION_API_KEY; 
 const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY; 
-let trackedUserIP = ''; //  set by user via input
+let trackedUserIP = ''; 
 
 // map by google map api ( intialise map ) -------------------------------------------------
 window.initMap = function() {
-    map = new google.maps.Map(document.getElementById('map'), { //consturctor call 
-        center: { lat: 0, lng: 0 },//initially
+    map = new google.maps.Map(document.getElementById('map'), { 
+        center: { lat: 0, lng: 0 },
         zoom: 2,
     });
    marker = new google.maps.Marker({
         
         map: map,
-        position: { lat: 0, lng: 0 }, // Initial position
+        position: { lat: 0, lng: 0 }, 
         title: 'Tracked Location',
     });
 }
@@ -28,17 +28,17 @@ window.fetchLocation = function() {
                     lng: parseFloat(data.longitude),
                 };
 
-                marker.setPosition(pos); // Update marker position
-                map.setCenter(pos);// view 
+                marker.setPosition(pos);
+                map.setCenter(pos); 
                 updateLocationDetails(data);
-                clearError(); // Clear error if location data is valid
+                clearError(); 
             } else {
                 displayError('Location data is missing...Try entering a valid IP address');
             }
         })
         .catch(error => console.error('Error fetching location:', error))
 }
-// Function to display error message
+
 function displayError(message) {
     const errorElement = document.getElementById('error-message');
     if (errorElement) {
@@ -46,7 +46,7 @@ function displayError(message) {
         errorElement.style.display = 'block';
     }
 }
-// Function to clear the error message
+
 function clearError() {
     const errorElement = document.getElementById('error-message');
     if (errorElement) {
@@ -60,21 +60,19 @@ window.updateLocationDetails = function(data) {
     document.getElementById('country').textContent = data.country_name || 'N/A';
 }
 
-// onclick --update IP button ------------------------------------------------------
+//update IP button ------------------------------------------------------
 window.updateIP = function() {
     const ipInput = document.getElementById('ipAddressInput').value;
     const blinkTextElements = document.getElementsByClassName('blink-text');
 
     if (ipInput) {
         trackedUserIP = ipInput;
-        fetchLocation(); // Updates location immediately after IP is set
-
-        // Hide the blinking message if IP is entered
+        fetchLocation(); 
+        
         if (blinkTextElements.length > 0) {
                  blinkTextElements[0].style.display = 'none';
         }
     } else {
-        // Show and update the blinking message if no IP address is entered
         if (blinkTextElements.length > 0) {
             blinkTextElements[0].style.display = 'block';
             blinkTextElements[0].textContent = 'Enter IP address here ⬆️ ';
@@ -84,7 +82,7 @@ window.updateIP = function() {
 
 
 // Load Google Maps script dynamically-----------------------------------------
-const script = document.createElement('script');//script tag created
+const script = document.createElement('script');
 script.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&callback=initMap&libraries=marker`;
 script.async = true;
 script.defer = true;
